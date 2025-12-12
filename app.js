879404a2aa7bdb789f932e10ff9039f7ba53eabb
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2025-12-12 11:26:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -531,6 +531,8 @@ const defaultModels = [
     { id: "IFC_EXA", src: "assets/modelo-15.xkt" },
     { id: "IFC_CLI", src: "assets/modelo-16.xkt" },
     { id: "IFC_EST_CT", src: "assets/modelo-17.xkt" },
+    { id: "IFC_ALI_220", src: "assets/modelo-18.xkt" },
+    { id: "IFC_ALI_380", src: "assets/modelo-19.xkt" },
 ];
 
 defaultModels.forEach(loadDefaultModel);
@@ -1314,6 +1316,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 
